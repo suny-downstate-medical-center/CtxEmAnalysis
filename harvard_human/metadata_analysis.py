068b81b547665d753fac5ceaf242cb5a5be4754d
craig <kelley92@bu.edu>
2022-03-24 15:01:26 -0400

    search connection files for instances of example cell

--- harvard_human/metadata_analysis.py
+++ harvard_human/metadata_analysis.py
@@ -7,6 +7,7 @@ from os import listdir
 from avro.datafile import DataFileReader
 from avro.io import DatumReader
 from matplotlib import pyplot as plt
+import multiprocessing
 
 # metadata = pd.read_pickle('h01_metadata.pkl')
 # metadata = pd.read_pickle('h01_metadata_c3.pkl')
@@ -38,6 +39,8 @@ metrics = ['nCell', 'Volume', 'avgVolume', 'stdVolume', 'avgNinExc', 'stdNinExc'
 vol_str = 'Volume (8x8x33nm voxels)'
 
 Vtissue = 1000 ** 3
+
+missing_somas = open('missing_somas.txt', 'w')
 
 data = {}
 not_in_soma_data = []
@@ -88,7 +91,6 @@ for cell in cells:
                     # soma_ind = np.argmax(skel.radii)
                     # data[cell][layer]['cell_locs'].append(list(skel.vertices[soma_ind]))
                 except:
-                    print('Cell ID: %s is missing from c3_rep_strict' % (celli.name))
                     try:
                         soma = soma_data[soma_data['c3_rep_manual'] == int(celli.name)]
                         x = int(soma.x.values)
@@ -96,11 +98,10 @@ for cell in cells:
                         z = int(soma.z.values)
                         data[cell][layer]['cell_locs'].append([x,y,z])
                         data[cell][layer]['cell_ids'].append(celli.name)
+                        somewhere.append(celli.name)
                         if cell.upper().replace('-','_') != soma['celltype']:
                             print('Cell ID: %s has cell type conflict' % (celli.name))
-                        somewhere.append(celli.name)
                     except:
-                        print('Cell ID: %s is missing from c3_rep_manual' % (celli.name))
                         try:
                             soma = soma_data[soma_data['c2_rep_strict'] == int(celli.name)]
                             x = int(soma.x.values)
@@ -112,7 +113,6 @@ for cell in cells:
                             if cell.upper().replace('-','_') != soma['celltype']:
                                 print('Cell ID: %s has cell type conflict' % (celli.name))
                         except:
-                            print('Cell ID: %s is missing from c2_rep_strict' % (celli.name))
                             try:
                                 soma = soma_data[soma_data['c2_rep_manual'] == int(celli.name)]
                                 x = int(soma.x.values)
@@ -124,10 +124,15 @@ for cell in cells:
                                 if cell.upper().replace('-','_') != soma['celltype']:
                                     print('Cell ID: %s has cell type conflict' % (celli.name))
                             except:
-                                print('Cell ID: %s is missing from c2_rep_manual' % (celli.name))
+                                missing_somas.write('Cell ID %s is missing somas.csv\n' % (celli.name))
                                 not_in_soma_data.append(celli.name)
 
     print('\n')
+
+missing_somas.close()
+
+print('%i cells from metadata.json found in somas.csv' % (len(somewhere)))
+print('%s cells from metadata.json not found in somas.csv\n' % (len(not_in_soma_data)))
 
 # compute neuronal volume fraction 
 Vol_nrn = 0
@@ -149,23 +154,60 @@ print('beta_nrn: ' + str(beta_nrn))
 # plt.ion()
 # plt.show()
 
+def findConns(input_data):
+    cell_id = input_data[0]
+    conn_file = input_data[1]
+    return_list = input_data[2]
+    reader = DataFileReader(open('./exported/'+conn_file, "rb"), 
+                DatumReader())
+    conns = []
+    for conn in reader:
+        if str(conn['pre_synaptic_site']['neuron_id']) == cell_id:
+            print('%s: found as presynaptic neuron' % (cell_id))
+            return_list.append(conn)
+        elif conn['pre_synaptic_site']['base_neuron_id'] == cell_id:
+            print('%s: presynaptic base neuron' % (cell_id))
+            return_list.append(conn)
+        elif str(conn['post_synaptic_partner']['neuron_id']) == cell_id:
+            print('%s: postsynaptic neuron' % (cell_id))
+            return_list.append(conn)
+    
+
 # look for synapse involving example cell
-# i = 5
-# layer = 'L4'
-# cell_id = data[cell][layer]['cell_ids'][0]
-# conn_files = listdir('./exported/')
-# conns = []
-# for conn_file in conn_files[:1]:
-#     if len(conn_file.split('.')) == 1:
-#         reader = DataFileReader(open('./exported/'+conn_file, "rb"), 
-#             DatumReader())
-#         for conn in reader:
-#             if str(conn['pre_synaptic_site']['neuron_id']) == cell_id:
-#                 print('presynaptic neuron')
-#                 conns.append(conn)
-#             elif conn['pre_synaptic_site']['base_neuron_id'] == cell_id:
-#                 print('presynaptic base neuron')
-#                 conns.append(conn)
-#             elif str(conn['post_synaptic_partner']['neuron_id']) == cell_id:
-#                 print('postsynaptic neuron')
-#                 conns.append(conn)
+manager = multiprocessing.Manager()
+conns = manager.list()
+
+# inds = [i for i in range(10)]
+inds = [0]
+layer = 'L4'
+conn_files = listdir('./exported/')
+
+input_data = []
+for ind in inds:
+    for conn_file in conn_files:
+        if len(conn_file.split('.')) == 1:
+            cell_id = data[cell][layer]['cell_ids'][ind]
+            input_data.append([cell_id, conn_file, conns])
+
+input_data = tuple(input_data)
+p = multiprocessing.Pool(6)
+p.map(findConns, input_data)
+
+# for ind in inds:
+#     cell_id = data[cell][layer]['cell_ids'][ind]
+#     conn_files = listdir('./exported/')
+#     conns = []
+#     for conn_file in conn_files[:1]:
+#         if len(conn_file.split('.')) == 1:
+#             reader = DataFileReader(open('./exported/'+conn_file, "rb"), 
+#                 DatumReader())
+#             for conn in reader:
+#                 if str(conn['pre_synaptic_site']['neuron_id']) == cell_id:
+#                     print('%s: found as presynaptic neuron' % (cell_id))
+#                     conns.append(conn)
+#                 elif conn['pre_synaptic_site']['base_neuron_id'] == cell_id:
+#                     print('%s: presynaptic base neuron' % (cell_id))
+#                     conns.append(conn)
+#                 elif str(conn['post_synaptic_partner']['neuron_id']) == cell_id:
+#                     print('%s: postsynaptic neuron' % (cell_id))
+#                     conns.append(conn)
